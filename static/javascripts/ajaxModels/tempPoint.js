94a354cf60cde9f739add737d7b21a15e1fe2de9
chrisvans <chrisvanschyndel@gmail.com>
2014-08-04 19:07:04 -0400

    JavaScript comments

--- static/javascripts/ajaxModels/tempPoint.js
+++ static/javascripts/ajaxModels/tempPoint.js
@@ -1,6 +1,8 @@
 function tempPointModel(options) {
     var tempPoint = {};
 
+    // Initialize own attributes based off of the object passed into the creation function
+    // You could consider 'tempPoint' to be 'self'
     tempPoint.__init__ = function(options) {
         tempPoint.roastProfileID = options.roastProfileID
         tempPoint.id = options.id
@@ -13,24 +15,37 @@ function tempPointModel(options) {
 
     tempPoint.__init__(options)
 
-    tempPoint.updatePointIcons = function() {
+    tempPoint.createPointIcon = function() {
 
+      // Find the index of the roastprofile line in visualization.data, based off of it's id
+      // seriesMap must be updated every time a new line is added, so this model knows which 
+      // data set it belongs to.
       var roastProfileIndex = seriesMap[tempPoint.roastProfileID]
 
-      console.log(lineChartVis.data[roastProfileIndex]);
-      console.log(tempPoint.id);
-      console.log(tempPoint.roastProfileID);
-
+      // If the line is not set to hidden, then we need to create comment icons on each point that has comments.
       if (!lineChartVis.data[roastProfileIndex].hidden) {
+
         var roastLineID = lineChartVis.data[roastProfileIndex].id.toString()
+        // Grab all the circles within our line's g group.
         var selectString = 'g.nv-group.nv-series-' + seriesMap[roastLineID] + ' > circle.nv-point'
+        // Attach the data to each circle.
         d3.selectAll(selectString).datum(lineChartVis.data);
+        // Iterate over each circle, passing in the data set.
+        // TODO: Attach each circle's own data to itself, rather than passing in the whole data array.
         d3.selectAll(selectString).each(function(d, i) { 
+
+          // Find the data value that corresponds to THIS tempPoint.
           if (d[roastProfileIndex].values[i].id == tempPoint.id) { 
+
+            // Update the data to reflect that this point has at least one comment.
             d[roastProfileIndex].values[i].hasComments = true;
+
             var commentIcon = d3.select('.svg-comment-icon.temppoint_' + tempPoint.id);
 
+            // If the node is null, we know that there was no icon previously.
             if (commentIcon.node() == null) {
+
+              // Create the icon, and attach it to the parent g with similiar x and y attributes to the circle it belongs to.
               var thisCircle = d3.select(this)
 
               var parentG = d3.select(thisCircle.node().parentNode)
@@ -64,14 +79,13 @@ function tempPointModel(options) {
         dataType: 'json',
         success: function(response) {
           tempPoint.commentCreateForm();
-          tempPoint.updatePointIcons();
+          tempPoint.createPointIcon();
 
         }
       })
     }
 
     // Deletes a comment
-
     tempPoint.commentDelete = function(commentID) {
       
       $.ajax({
@@ -110,13 +124,16 @@ function tempPointModel(options) {
 
           $('#comments').html(response.data);
 
+          // Setup click handler for the submit button
           $('#submit-pointcomment').click(function() {tempPoint.commentCreate()})
 
+          // Setup click handler for the delete button.
           $('.comment-delete').click(function() {
             commentID = $(this).closest('div.comment').prop('id');
             tempPoint.commentDelete(commentID);
           })
 
+          // Scroll the screen down to the comments div.
           var target= "div.comments";
           $('html, body').animate({
             scrollTop: $(target).offset().top
