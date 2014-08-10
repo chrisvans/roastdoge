50213439e5c7fe865431054fc74a91fe246a0d05
chrisvans <chrisvanschyndel@gmail.com>
2014-08-09 23:24:50 -0400

    Test

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
+	import pdb; pdb.set_trace()
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.base")
 
     from django.core.management import execute_from_command_line
